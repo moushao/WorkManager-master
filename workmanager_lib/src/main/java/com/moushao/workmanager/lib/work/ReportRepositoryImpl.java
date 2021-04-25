10bfc30613d3ab35bdcb034443bac9845f457de2
moushao <moushao1990@gmail.com>
2021-04-25 23:45:48 +0800

    assert retry

--- workmanager_lib/src/main/java/com/moushao/workmanager/lib/work/ReportRepositoryImpl.java
+++ workmanager_lib/src/main/java/com/moushao/workmanager/lib/work/ReportRepositoryImpl.java
@@ -1,7 +1,11 @@
 package com.moushao.workmanager.lib.work;
 
+import android.util.Log;
+
 import com.moushao.workmanager.lib.ReportRepository;
 import com.moushao.workmanager.lib.WorkResult;
+
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Single;
 import io.reactivex.annotations.NonNull;
@@ -12,9 +16,10 @@ public class ReportRepositoryImpl implements ReportRepository {
     @Override
     public Single<Integer> report() {
         // todo report device permission info
-        return  Single.just(1).map(new Function<Integer, Integer>() {
+        return  Single.just(1).delay(10, TimeUnit.SECONDS).map(new Function<Integer, Integer>() {
             @Override
             public Integer apply(@NonNull Integer integer) throws Exception {
+                Log.e("ReportWork", "apply");
                 return WorkResult.SUCCESS;
             }
         });
